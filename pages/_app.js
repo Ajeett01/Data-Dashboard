20ab82029c3058807595ae5c6e74e34691a251d3
Ajeett01 <abhitiwari501@gmail.com>
2024-04-29 20:29:58 +0530

    Comment

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '@/styles/globals.css';
 import Sidebar from '../components/Sidebar';
 
+// App.js default code 
+
 export default function App({ Component, pageProps }) {
   return (
     <Sidebar>
